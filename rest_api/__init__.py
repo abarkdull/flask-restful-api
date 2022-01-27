e488a6607e14faa50758507e3759c735935ac713
abarkdull <abarkdull@gmail.com>
2022-01-27 07:45:07 -0700

    add comments

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -18,6 +18,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 api = Api(app)
 
+
+# before first request, create database
 @app.before_first_request
 def create_tables():
     db.create_all()
